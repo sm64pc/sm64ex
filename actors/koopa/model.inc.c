345ac387a7d1d7bcdaff57793f29e3d1165dd23a
Colton Rushton <colton51919@gmail.com>
2020-05-15 17:16:02 -0300

    Adds burn smoke and KtQ fixes.

--- actors/koopa/model.inc.c
+++ actors/koopa/model.inc.c
@@ -2077,8 +2077,8 @@ const Gfx koopa_seg6_dl_0600C498[] = {
     gsSPVertex(koopa_seg6_vertex_0600B560, 9, 0),
     gsSP2Triangles( 0,  1,  2, 0x0,  3,  4,  5, 0x0),
     gsSP1Triangle( 6,  7,  8, 0x0),
-    gsSPLight(koopa_seg6_texture_06002648 + 0x20, 1), // this malformed light results in a
-    gsSPLight(koopa_seg6_texture_06002648 + 0x18, 2), // koopa appearing to wear pink shorts.
+    gsSPLight(&koopa_seg6_lights_06002630.l, 1),
+    gsSPLight(&koopa_seg6_lights_06002630.a, 2),
     gsSPVertex(koopa_seg6_vertex_0600B5F0, 15, 0),
     gsSP2Triangles( 0,  1,  2, 0x0,  3,  4,  5, 0x0),
     gsSP2Triangles( 6,  7,  0, 0x0,  8,  5,  9, 0x0),
