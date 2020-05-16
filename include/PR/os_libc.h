e42e1587b24c7042de03cf00a715f6fcbd4a8fb4
Colton G. Rushton <colton51919@gmail.com>
2020-05-16 17:14:10 -0300

    MXE fix for os_libc.h

--- include/PR/os_libc.h
+++ include/PR/os_libc.h
@@ -4,7 +4,13 @@
 #include "ultratypes.h"
 
 // Old deprecated functions from strings.h, replaced by memcpy/memset.
+#ifdef NO_BZERO
+#include <string.h>
+#define bzero(buf, size) memset(buf, 0, size)
+#define bcopy(src, dst, size) memcpy(dst, src, size)
+#else
 extern void bcopy(const void *, void *, size_t);
 extern void bzero(void *, size_t);
+#endif
 
 #endif /* !_OS_LIBC_H_ */
