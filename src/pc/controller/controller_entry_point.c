0969b7d9bf2b75b80f735a0b954924cec620054c
Colton G. Rushton <colton51919@gmail.com>
2020-05-18 01:18:47 -0300

    Proper fix to controller axis invert issue

--- src/pc/controller/controller_entry_point.c
+++ src/pc/controller/controller_entry_point.c
@@ -41,8 +41,8 @@ void osContGetReadData(OSContPad *pad) {
 #ifdef BETTERCAMERA
     uint32_t magnitude_sq = (uint32_t)(rightx * rightx) + (uint32_t)(righty * righty);
     if (magnitude_sq > (uint32_t)(DEADZONE * DEADZONE)) {
-        c_rightx = rightx / 0x100;
-        int stick_y = -righty / 0x100;
+        c_rightx = -rightx / 0x100;
+        int stick_y = righty / 0x100;
         c_righty = stick_y == 128 ? 127 : stick_y;
     } else
     {
