7693e32f9e7e0b25059956395c16ad97558bdbe5
Colton G. Rushton <colton51919@gmail.com>
2020-05-18 10:00:27 -0300

    This fix is now redundant

--- src/game/main.h
+++ src/game/main.h
@@ -73,6 +73,6 @@ extern s8 gShowDebugText;
 extern void set_vblank_handler(s32 a, struct VblankHandler *b, OSMesgQueue *queue, OSMesg *msg);
 extern void dispatch_audio_sptask(struct SPTask *spTask);
 extern void send_display_list(struct SPTask *a);
-extern int main(void);
+extern void main(void);
 
 #endif
