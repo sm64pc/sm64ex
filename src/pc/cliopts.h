ee795fa7af4baf6118004fda6d9b02fb44a1c6dc
yksoft1 <yksoft1@gmail.com>
2020-05-19 20:15:25 +0800

    My hacks to cliopts.h is not necessary anymore.

--- src/pc/cliopts.h
+++ src/pc/cliopts.h
@@ -1,13 +1,14 @@
-#include <PR/ultratypes.h>
-#include "sm64.h"
+#ifndef _CLIOPTS_H
+#define _CLIOPTS_H
 
-struct PCCLIOptions 
-{
-	u8 SkipIntro;
-    u8 FullScreen;
-    char * ConfigFile;
+struct PCCLIOptions  {
+    unsigned int SkipIntro;
+    unsigned int FullScreen;
+    char ConfigFile[1024];
 };
 
 extern struct PCCLIOptions gCLIOpts;
 
 void parse_cli_opts(int argc, char* argv[]);
+
+#endif // _CLIOPTS_H
