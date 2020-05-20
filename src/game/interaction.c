ab72f3e1b4e22dce6d82ebef28175df9c65e0d31
GateGuy <57763469+GateGuy@users.noreply.github.com>
2020-05-20 00:39:12 -0400

    Added cheat: Stay in level after star
    Added a cheat that makes you stay in a level after collecting a star. You can hold L while collecting a star to temporarily disable this effect. This cheat has no effect on Bowser keys, or stars that already make you stay in the level in the base game.
    
    Also, if you have a better name than "Stay in level after star", be my guest. "Stay in level after getting a star (hold L to cancel)" doesn't fit in the cheat description.

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -21,6 +21,7 @@
 #include "seq_ids.h"
 #include "course_table.h"
 #include "thread6.h"
+#include "pc/cheats.h"
 
 #define INT_GROUND_POUND_OR_TWIRL (1 << 0) // 0x01
 #define INT_PUNCH                 (1 << 1) // 0x02
@@ -766,6 +767,10 @@ u32 interact_star_or_key(struct MarioState *m, UNUSED u32 interactType, struct O
     u32 starIndex;
     u32 starGrabAction = ACT_STAR_DANCE_EXIT;
     u32 noExit = (o->oInteractionSubtype & INT_SUBTYPE_NO_EXIT) != 0;
+    if (Cheats.StayInLevel == TRUE && Cheats.EnableCheats == TRUE && !(m->controller->buttonDown & L_TRIG) &&
+        !(gCurrLevelNum == LEVEL_BOWSER_1 || gCurrLevelNum == LEVEL_BOWSER_2 || gCurrLevelNum == LEVEL_BOWSER_3)) {
+        noExit = TRUE;
+    }
     u32 grandStar = (o->oInteractionSubtype & INT_SUBTYPE_GRAND_STAR) != 0;
 
     if (m->health >= 0x100) {
