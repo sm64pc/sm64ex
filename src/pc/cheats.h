9917188ec84b07aec2e45f22d2fc9ce27457ec60
Zerocker <zerocloude@gmail.com>
2020-05-31 14:18:17 +0900

    Revert "Secret Cheat was added"
    This reverts commit 19990cde389aac999290483ee2f0070e28df9508.

--- src/pc/cheats.h
+++ src/pc/cheats.h
@@ -13,10 +13,6 @@ struct CheatList {
     bool         ExitAnywhere;
     bool         HugeMario;
     bool         TinyMario;
-    bool         Unknown1;
-    bool         Unknown2;
-    bool         Unknown3;
-    bool         DisableBLJ;
 };
 
 extern struct CheatList Cheats;
