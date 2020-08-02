feab74a634b257019b0550bb081440ad42b20d53
Martin Pham <tuyentq2009@gmail.com>
2020-08-02 16:15:03 +0200

    Update options_menu.c

--- src/game/options_menu.c
+++ src/game/options_menu.c
@@ -280,6 +280,7 @@ static struct Option optsCheats[] = {
     DEF_OPT_TOGGLE( optsCheatsStr[6], &Cheats.ExitAnywhere ),
     DEF_OPT_TOGGLE( optsCheatsStr[7], &Cheats.HugeMario ),
     DEF_OPT_TOGGLE( optsCheatsStr[8], &Cheats.TinyMario ),
+    DEF_OPT_TOGGLE( optsCheatsStr[9], &Cheats.ForeverCap ),
 
 };
 
