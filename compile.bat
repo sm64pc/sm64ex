7f60cb1f94217d406fb3d92249418bcf3473e5a9
John Collins <ineedhelpbad@gmail.com>
2020-08-15 10:48:08 -0500

    Added option to select camera mode on bowser stages.

---
+++ compile.bat
@@ -0,0 +1,2 @@
+del %~dp0build\us_pc\sm64.us.f3dex2e.exe
+C:\msys64\mingw64.exe make -j9 BETTERCAMERA=1 NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1
--- src/game/options_menu.c
+++ src/game/options_menu.c
@@ -70,6 +70,7 @@ static const u8 optsCameraStr[][32] = {
     { TEXT_OPT_MOUSE },
     { TEXT_OPT_CAMD },
     { TEXT_OPT_CAMON },
+     {TEXT_OPT_BOWSERCAM},
 };
 
 static const u8 optsVideoStr[][32] = {
@@ -229,6 +230,7 @@ static struct Option optsCamera[] = {
     DEF_OPT_SCROLL( optsCameraStr[4], &configCameraAggr, 0, 100, 1 ),
     DEF_OPT_SCROLL( optsCameraStr[5], &configCameraPan, 0, 100, 1 ),
     DEF_OPT_SCROLL( optsCameraStr[8], &configCameraDegrade, 0, 100, 1 ),
+    DEF_OPT_TOGGLE( optsCameraStr[10], &configBowserCam ),
 };
 #endif
 
