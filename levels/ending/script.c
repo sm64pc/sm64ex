fbb508043fb5ee3e9247325e659567e62c61d7b2
GateGuy <57763469+GateGuy@users.noreply.github.com>
2020-09-09 20:00:12 -0400

    Changed transition from title to Mario face

--- levels/ending/script.c
+++ levels/ending/script.c
@@ -37,5 +37,5 @@ const LevelScript level_ending_entry[] = {
     /*17*/ CALL_LOOP(/*arg*/ 1, /*func*/ credits_end_wait_for_reset),
     /*18*/ TRANSITION(/*transType*/ WARP_TRANSITION_FADE_INTO_COLOR, /*time*/ 75, /*color*/ 0x00, 0x00, 0x00),
     /*19*/ SLEEP(/*frames*/ 120),
-    /*20*/ EXECUTE(/*seg*/ 0x14, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_intro_entry_1),
+    /*20*/ EXECUTE(/*seg*/ 0x14, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_intro_entry_2),
 };
