c59074ae15fd88ff68d84966f8e785748a65ccd6
NoHomoBoi <36680385+KiritoDv@users.noreply.github.com>
2020-09-12 21:08:51 -0500

    Camera fix from sm64ex

--- src/game/behaviors/snufit.inc.c
+++ src/game/behaviors/snufit.inc.c
@@ -179,12 +179,7 @@ void bhv_snufit_loop(void) {
  */
 void bhv_snufit_balls_loop(void) {
     // If far from Mario or in a different room, despawn.
-    if ((o->activeFlags & ACTIVE_FLAG_IN_DIFFERENT_ROOM)
-#ifndef NODRAWINGDISTANCE
-        || (o->oTimer != 0 && o->oDistanceToMario > 1500.0f)) {
-#else
-        || (o->oTimer != 0)) {
-#endif
+    if ((o->activeFlags & ACTIVE_FLAG_IN_DIFFERENT_ROOM) || (o->oTimer != 0 && o->oDistanceToMario > 1500.0f)) {
         obj_mark_for_deletion(o);
     }
 
--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -1754,7 +1754,7 @@ s32 act_flying(struct MarioState *m) {
         set_camera_mode(m->area->camera, CAMERA_MODE_BEHIND_MARIO, 1);
 #else
         if (newcam_active == 0)
-            set_camera_mode(m->area->camera, m->area->camera->defMode, 1);
+            set_camera_mode(m->area->camera, CAMERA_MODE_BEHIND_MARIO, 1);
         else
         {
             m->area->camera->mode = CAMERA_MODE_NEWCAM;
