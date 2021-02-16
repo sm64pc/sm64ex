44f9e94d9fb5628bfa86f880216f99e61b046168
Colton Rushton <colton51919@gmail.com>
2021-02-16 10:00:42 -0400

    Made the let's-a-go fix a QoL fix

--- levels/menu/script.c
+++ levels/menu/script.c
@@ -88,7 +88,11 @@ const LevelScript level_main_menu_entry_2[] = {
     /*37*/ TRANSITION(/*transType*/ WARP_TRANSITION_FADE_INTO_COLOR, /*time*/ 16, /*color*/ 0xFF, 0xFF, 0xFF),
     /*39*/ SLEEP(/*frames*/ 16),
     /*40*/ CLEAR_LEVEL(),
+    #ifndef QOL_FIXES
+    /*41*/ SLEEP_BEFORE_EXIT(/*frames*/ 1),
+    #else
     /*41*/ SLEEP_BEFORE_EXIT(/*frames*/ 5),
+    #endif
     // L1:
     /*42*/ EXIT(),
 };
