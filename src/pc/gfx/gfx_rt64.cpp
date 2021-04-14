d2a0a239225a480440fd08b81f1bad56e2db4410
Dario <dariosamo@gmail.com>
2021-04-13 22:04:26 -0300

    Light up the ghost house.

--- src/pc/gfx/gfx_rt64.cpp
+++ src/pc/gfx/gfx_rt64.cpp
@@ -46,7 +46,7 @@ using json = nlohmann::json;
 #define MAX_LIGHTS						512
 #define MAX_LEVELS						39
 #define MAX_AREAS						3
-#define MAX_LEVEL_LIGHTS				64
+#define MAX_LEVEL_LIGHTS				128
 #define LEVEL_LIGHTS_FILENAME			FS_BASEDIR "/rt64/level_lights.json"
 #define LIGHT_SAMPLE_PRESETS_FILENAME	FS_BASEDIR "/rt64/light_sample_presets.json"
 #define GEO_LAYOUT_MODS_FILENAME		FS_BASEDIR "/rt64/geo_layout_mods.json"
