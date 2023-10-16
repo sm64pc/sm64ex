98aa0fe3762df5d97e65f1986f15dfea33d098b5
fgsfds <derp.primus@gmail.com>
2023-10-16 15:03:20 +0200

    fix #537

--- src/pc/gfx/gfx_opengl.c
+++ src/pc/gfx/gfx_opengl.c
@@ -529,7 +529,7 @@ static void gfx_opengl_select_texture(int tile, GLuint texture_id) {
      gfx_opengl_set_texture_uniforms(opengl_prg, tile);
 }
 
-static void gfx_opengl_upload_texture(uint8_t *rgba32_buf, int width, int height) {
+static void gfx_opengl_upload_texture(const uint8_t *rgba32_buf, int width, int height) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba32_buf);
     opengl_tex[opengl_curtex]->size[0] = width;
     opengl_tex[opengl_curtex]->size[1] = height;
